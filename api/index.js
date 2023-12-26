114fa76ee33c98874b25e7ea62c62a307609e911
Fovwe <afovwe@gmail.com>
2023-12-27 00:42:08 +0100

    Create test api route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.route.js';
 dotenv.config();
-mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log('Connected to MongoDB!');
-}).catch((err) => {
-    console.log(err);
-});
 
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(7000, () => {
-    console.log('Server running on port 7000!!!');
-}
-);
+  console.log('Server running on port 7000!!!');
+});
+
+// Fix: Add a leading slash before "api/user"
+app.use("/api/user", userRouter);
