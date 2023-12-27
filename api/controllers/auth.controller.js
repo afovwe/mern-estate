140410eca881dbdc28fb5e35819bb492789ca016
Fovwe <afovwe@gmail.com>
2023-12-27 01:52:30 +0100

    signup api route created

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+
+import User from "../models/users.model.js";
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res)=> {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({username, email, password: hashedPassword});
+    try {
+        await newUser.save()
+    res.status(201).json('Sign Up Successfully!');     
+       //console.log(req.body);
+
+    } catch (error) {
+        res.status(500).json(error.message);
+        
+    }
+   
+};   
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO)
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(7000, () => {
   console.log('Server running on port 7000!!!');
@@ -20,3 +22,5 @@ app.listen(7000, () => {
 
 // Fix: Add a leading slash before "api/user"
 app.use("/api/user", userRouter);
+// Fix: Add a leading slash before "api/user"
+app.use("/api/auth", authRouter);
