cfa9e630b738f814679ca7e41b5a613d859a2185
Fovwe <afovwe@gmail.com>
2023-12-28 20:00:29 +0100

    Update the header and mak the profile page protected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from "./pages/signUp";
 import About from "./pages/about";
 import Profile from "./pages/profile";
 import Header from "./components/Header";
-
+import PrivateRoute from './components/PrivateRoute';
 
 
 export default function App() {
@@ -16,7 +16,10 @@ export default function App() {
     <Route path="/sign-in" element={<SignIn />} />
     <Route path="/sign-up" element={<SignUp />} />
     <Route path="/about" element={<About />} />
-    <Route path="/profile" element={<Profile />} />
+    
+       <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />      
+        </Route>
   </Routes>
   </BrowserRouter>
 }
