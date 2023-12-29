c16a4f125c3cd1a855fad763ada4f969b033ab4e
Fovwe <afovwe@gmail.com>
2023-12-29 23:02:24 +0100

    add create listing api routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -21,10 +22,10 @@ app.listen(7000, () => {
   console.log('Server running on port 7000!!!');
 });
 
-// Fix: Add a leading slash before "api/user"
+
 app.use("/api/user", userRouter);
-// Fix: Add a leading slash before "api/user"
 app.use("/api/auth", authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
