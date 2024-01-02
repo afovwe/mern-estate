a0663878295c1554895637073ef072440211c3bd
Fovwe <afovwe@gmail.com>
2024-01-02 14:13:25 +0100

    changed signin to signIn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home";
-import SignIn from "./pages/signin";
+import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 import About from "./pages/about";
 import Profile from "./pages/profile";
